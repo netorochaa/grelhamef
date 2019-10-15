127a7d9510564633f9b8d6a82e34b5bec2b0531e
netorochaa <jrr.neto@hotmail.com>
2019-10-14 22:43:12 -0300

    Organização no código; Implementação da rigidezLocalElemento1 e 2, MatrizRigidezEmCoordenadasLocais e inicializaMatrizRotacao

--- Grelha MEF/Form1.cs
+++ Grelha MEF/Form1.cs
@@ -12,17 +12,39 @@ namespace Grelha_MEF
 {
     public partial class Form1 : Form
     {   
+        //Matriz de rigidez local
+        //elemento 1
+        public static int[,] rigidezLocalElemento1 = new int[6,6] 
+            {  
+                {11, 12, 13, 14, 15, 16},
+                {21, 22, 23, 24, 25, 26},
+                {31, 32, 33, 34, 35, 36},
+                {41, 42, 43, 44, 45, 46},
+                {51, 52, 53, 54, 55, 56},
+                {61, 62, 63, 64, 65, 66}
+            };
+        //elemento 2
+        public static int[,] rigidezLocalElemento2 = new int[6,6] 
+            {  
+                {11, 12, 13, 14, 15, 16},
+                {21, 22, 23, 24, 25, 26},
+                {31, 32, 33, 34, 35, 36},
+                {41, 42, 43, 44, 45, 46},
+                {51, 52, 53, 54, 55, 56},
+                {61, 62, 63, 64, 65, 66}
+            };
+
         //Graus de liberdade (associando local a global)
         //no 1
-        public static int[] sistemaGlobalGrausLiberdadeNo1  = new int[] { 1, 2, 3 };
+        public static int[] sistemaGlobalGrausLiberdadeNo1 = new int[] { 1, 2, 3 };
         public static int[] sistemaLocalElemento1No1       = new int[] { 1, 2, 3 };
         public static int[] sistemaLocalElemento2No1       = new int[] { 1, 2, 3 };
         //no 2
-        public static int[] sistemaGlobalGrausLiberdadeNo2  = new int[] { 4, 5, 6 };
+        public static int[] sistemaGlobalGrausLiberdadeNo2 = new int[] { 4, 5, 6 };
         public static int[] sistemaLocalElemento1No2       = new int[] { 0, 0, 0 };
         public static int[] sistemaLocalElemento2No2       = new int[] { 4, 5, 6 };
         //no 3
-        public static int[] sistemaGlobalGrausLiberdadeNo3  = new int[] { 7, 8, 9 };
+        public static int[] sistemaGlobalGrausLiberdadeNo3 = new int[] { 7, 8, 9 };
         public static int[] sistemaLocalElemento1No3       = new int[] { 4, 5, 6 };
         public static int[] sistemaLocalElemento2No3       = new int[] { 0, 0, 0 };
         //item 1: graus de liberdade
@@ -40,16 +62,16 @@ namespace Grelha_MEF
         public static int quantidadeLocalElemento1 = sistemaLocalElemento1No1.Length + sistemaLocalElemento1No2.Length + sistemaLocalElemento2No3.Length;
         public static int[] localElemento1         = new int[quantidadeLocalElemento1];
         //Cria matriz local do elemento 2
-        string[,] matrizLocalElemeto1 = new string[grausLiberdadeGlobal.Length, grausLiberdadeGlobal.Length];
+        string[,] matrizLocalSistemaGlobalElemeto1 = new string[grausLiberdadeGlobal.Length, grausLiberdadeGlobal.Length];
 
         //Vetores totais do elemento 2
         public static int quantidadeLocalElemento2 = sistemaLocalElemento2No1.Length + sistemaLocalElemento2No2.Length + sistemaLocalElemento2No3.Length;
         public static int[] localElemento2         = new int[quantidadeLocalElemento2];
         //Cria matriz local do elemento 2
-        string[,] matrizLocalElemeto2 = new string[grausLiberdadeGlobal.Length, grausLiberdadeGlobal.Length];
+        string[,] matrizLocalSistemaGlobalElemeto2 = new string[grausLiberdadeGlobal.Length, grausLiberdadeGlobal.Length];
 
         //Matriz global dos elementos
-        string[,] matrizGlobalElemetos = new string[grausLiberdadeGlobal.Length, grausLiberdadeGlobal.Length];
+        string[,] matrizGlobalEstrutura = new string[grausLiberdadeGlobal.Length, grausLiberdadeGlobal.Length];
 
         //Associando nó global a nó local
         //elemento 1
@@ -57,11 +79,7 @@ namespace Grelha_MEF
         public static int[] noGlobalElemento1 = { 1, 3 };
         //elemento 2
         public static int[] noLocalElemento2  = { 1, 2 };
-        public static int[] noGlobalElemento2 = { 1, 2 };
-        //item 1: nó local do elemento
-        //item 2: nó global do elemento
-        public static Tuple<int[], int[]> elemento1AssociandoGlobalALocal = new Tuple<int[], int[]>(noLocalElemento1, noGlobalElemento1);
-        public static Tuple<int[], int[]> elemento2AssociandoGlobalALocal = new Tuple<int[], int[]>(noLocalElemento2, noGlobalElemento2);
+        public static int[] noGlobalElemento2 = { 1, 2 };        
 
         public Form1()
         {
@@ -95,9 +113,9 @@ namespace Grelha_MEF
                 {
                     //percorre elementos
                     if (!localElemento1[j].Equals(0) && !localElemento1[i].Equals(0))
-                        matrizLocalElemeto1[i, j] = "K" + localElemento1[i] + "," + localElemento1[j] + "(1)";
+                        matrizLocalSistemaGlobalElemeto1[i, j] = "K" + localElemento1[i] + "," + localElemento1[j] + "(1)";
                     else
-                        matrizLocalElemeto1[i, j] = "-";
+                        matrizLocalSistemaGlobalElemeto1[i, j] = "-";
                     //Console.WriteLine("\r\nIndice [" + (i + 1) + ", " + (j + 1) + "] " + matrizLocalElemeto1[i, j]);
                 }
             }
@@ -111,9 +129,9 @@ namespace Grelha_MEF
                 {
                     //percorre elementos
                     if (!localElemento2[j].Equals(0) && !localElemento2[i].Equals(0))
-                        matrizLocalElemeto2[i, j] = "K" + localElemento2[i] + "," + localElemento2[j] + "(2)";
+                        matrizLocalSistemaGlobalElemeto2[i, j] = "K" + localElemento2[i] + "," + localElemento2[j] + "(2)";
                     else
-                        matrizLocalElemeto2[i, j] = "-";
+                        matrizLocalSistemaGlobalElemeto2[i, j] = "-";
                     //Console.WriteLine("\r\nIndice [" + (i + 1) + ", " + (j + 1) + "] " + matrizLocalElemeto2[i, j]);
                 }
             }
@@ -126,22 +144,65 @@ namespace Grelha_MEF
                 for (int j = 0; j < grausLiberdadeGlobal.Length; j++)
                 {                    
                     //percorre elementos
-                    if (!matrizLocalElemeto1[i, j].Equals("-") && !matrizLocalElemeto2[i, j].Equals("-"))
-                        matrizGlobalElemetos[i, j] = matrizLocalElemeto1[i, j] + "+" + matrizLocalElemeto2[i, j];
-                    else if (!matrizLocalElemeto1[i, j].Equals("-") && matrizLocalElemeto2[i, j].Equals("-"))                    
-                        matrizGlobalElemetos[i, j] = matrizLocalElemeto1[i, j];
-                    else if (matrizLocalElemeto1[i, j].Equals("-") && !matrizLocalElemeto2[i, j].Equals("-") )
-                        matrizGlobalElemetos[i, j] = matrizLocalElemeto2[i, j];
+                    if (!matrizLocalSistemaGlobalElemeto1[i, j].Equals("-") && !matrizLocalSistemaGlobalElemeto2[i, j].Equals("-"))
+                        matrizGlobalEstrutura[i, j] = matrizLocalSistemaGlobalElemeto1[i, j] + "+" + matrizLocalSistemaGlobalElemeto2[i, j];
+                    else if (!matrizLocalSistemaGlobalElemeto1[i, j].Equals("-") && matrizLocalSistemaGlobalElemeto2[i, j].Equals("-"))
+                        matrizGlobalEstrutura[i, j] = matrizLocalSistemaGlobalElemeto1[i, j];
+                    else if (matrizLocalSistemaGlobalElemeto1[i, j].Equals("-") && !matrizLocalSistemaGlobalElemeto2[i, j].Equals("-"))
+                        matrizGlobalEstrutura[i, j] = matrizLocalSistemaGlobalElemeto2[i, j];
                     else
-                        matrizGlobalElemetos[i, j] = "0";
+                        matrizGlobalEstrutura[i, j] = "0";
 
                     //matrizGlobalElemetos[i, j] = matrizLocalElemeto1[i, j] + " + " + matrizLocalElemeto2[i, j];
                     //else
                     //    matrizGlobalElemetos[i, j] = "0";
-                    Console.WriteLine("Indice [" + (i + 1) + ", " + (j + 1) + "] " + matrizGlobalElemetos[i, j]);
+                    Console.WriteLine("Indice [" + (i + 1) + ", " + (j + 1) + "] " + matrizGlobalEstrutura[i, j]);
                 }
             }
         }
 
+        //**INSERIR BOTÃO PARA INICIALIZAR ESSA MATRIZ**
+        public void inicializaMatrizRigidezEmCoordenadasLocais(double b, double h, double moduloYoung, double coeficientePoisson, double L1, double L2)
+        {
+            //Calculando Separadamente
+            double I = (b * Math.Pow(h, 3))/12;
+            //string unidadeMedidaI = "m⁴";
+
+            double E = moduloYoung * Math.Pow(10, 9);
+            //string unidadeMedidaE = "kN/m²";
+
+            double V = coeficientePoisson;
+
+            double G = E / (2 * (1 + V));
+            //string unidadeMedidaG = "kN/m²";
+
+            double J = h * Math.Pow(b, 3) * ((1 / 3) - 0.21 * (b / h) * (1 - (Math.Pow(b, 4) / (12 * Math.Pow(h, 4)))));
+            //string unidadeMedidaJ = "m⁴";
+
+            //Matriz de rigidez do elemento em coordenadas locais
+            double[,] matrizRigidezElementoEmCoordenadasLocais = new double[6, 6]
+                {
+                    {(12*E*I)/Math.Pow(L1, 3), 0, (G*E*I)/Math.Pow(L1, 2), (-12*E*I)/Math.Pow(L1, 3), 0, (6*E*I)/Math.Pow(L1, 2)},
+                    {0, (G*J)/L1, 0, 0, (-G*J)/L1, 0},
+                    {(6*E*I)/Math.Pow(L1, 2), 0, (4*E*I)/L1, (-6*E*I)/Math.Pow(L1, 2), 0, (2*E*I)/L1},
+                    {(-12*E*I)/Math.Pow(L1, 3), 0, (-6*E*I)/Math.Pow(L1, 2), (12*E*I)/Math.Pow(L1, 3), 0, 0},
+                    {0, (-G*J)/L1, 0, 0, (G*J)/L1, 0},
+                    {(6*E*I)/Math.Pow(L1, 2), 0, (2*E*I)/L1, (-6*E*I)/Math.Pow(L1, 2), 0, (4*E*I)/L1}
+                };
+        }
+
+        //**INSERIR BOTÃO PARA INICIALIZAR ESSA MATRIZ**
+        public void inicializaMatrizRotacao(double conseno, double seno)
+        {
+            double[,] matrizRotacao = new double[6, 6]
+                {
+                    {1, 0, 0, 0, 0, 0},
+                    {0, conseno, seno, 0, 0, 0},
+                    {0, -seno, conseno, 0, 0, 0},
+                    {0, 0, 0, 1, 0, 0},
+                    {0, 0, 0, 0, conseno, seno},
+                    {0, 0, 0, 0, -seno, conseno}
+                };
+        }
     }
 }
